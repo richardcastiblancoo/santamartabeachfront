b200fc16b0685b5261dd1a65763ce151fc412cae
richardcastiblancoo <richardcastiblanco8@gmail.com>
2026-01-18 20:04:29 -0500

    logo-2

--- index.php
+++ index.php
@@ -101,8 +101,6 @@
     <link href="https://fonts.googleapis.com" rel="preconnect" />
     <link crossorigin="" href="https://fonts.gstatic.com" rel="preconnect" />
     <link rel="stylesheet" href="/css/style.css">
-    <link rel="shortcut icon" href="" type="image/x-icon">
-
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&amp;display=swap" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&amp;display=swap" rel="stylesheet" />
     <!-- los scripts -->
